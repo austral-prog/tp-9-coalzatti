3150e133ebe402cd599be6f675a7deafb56f3aef
Constanza Sofía Alzatti <162724190+coalzatti@users.noreply.github.com>
2024-06-07 08:16:23 -0300

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -1,41 +1,41 @@
 def create_inventory(items):
     inventory = dict()
     for item in items:
-        if item in inventory:
-            inventory[item] += 1
-        else:
-            inventory[item] ==1
-    return inventory
-
-   
-
-
-def add_items(inventory, items):
-    for item in items:
-        if item in inventory:
-            inventory[item] += 1
-        else:
-            inventory[item] ==1
+        counter = 0
+        for k in items:
+            if item == k:
+                counter += 1
+                inventory[item]= counter  
     return inventory
     
- 
 
+def add_items(inventory, items):
+    for item in set(items):
+        if item in inventory:
+            counter = items.count(item)
+            inventory[item] += counter
+        else:
+            inventory[item] = items.count(i)
+    return inventory
 
 def decrement_items(inventory, items):
-   for item in items:
-        if item in inventory  and inventory > 0:
-            inventory[item] -= 1
-        return inventory
+    for item in set(items):
+        if inventory[item] != 0:
+            inventory[item] -= items.count(i)
+        if inventory[item] < 0:
+            inventory[item] = 0
+    return inventory
 
 
 def remove_item(inventory, item):
-    if item in inventory:
-        del inventory[item]
-    else:
-        return inventory
+    if item in inventory.keys():
+         del inventory[item]
+    return inventory
+
 
 def list_inventory(inventory):
-     for item in items:
-        if item in inventory  and inventory > 0:
-            return [inventory]
-
+    new_list = []
+    for k,v in inventory.items():
+        if v > 0:
+            new_list.append((k,v))
+    return new_list
